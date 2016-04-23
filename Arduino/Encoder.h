ab55c31af50547141c6db956254a1e694ccd670d
Greg Cunningham <greg@curioustech.net>
2016-04-23 11:50:35 -0400

    Create Encoder.h

---
+++ Arduino/Encoder.h
@@ -0,0 +1,20 @@
+#ifndef ENCODER_H
+#define ENCODER_H
+#include <arduino.h>
+
+class Encoder
+{
+public:
+  	Encoder(int8_t aPin, int8_t bPin, void (*callbackr)());
+    int poll(void);
+    int read(void);
+    void isr(void);
+private:
+    void (*isrCallback)(void);
+    volatile int8_t m_ticks;
+    volatile int8_t m_dir;
+    volatile int8_t m_aPin, m_bPin;
+    int8_t m_lastA;
+};
+
+#endif // ENCODER_H
