9cdade0572d0ebcf4380befff2903512ba1d3a11
Greg Cunningham <greg@curioustech.net>
2016-04-23 21:19:44 -0400

    Create WebHandler.h

---
+++ Arduino/WebHandler.h
@@ -0,0 +1,22 @@
+#ifndef WEBHANDLER_H
+#define WEBHANDLER_H
+
+#include <Arduino.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+
+void startServer(void);
+void handleServer(void);
+void secondsServer(void);
+String ipString(IPAddress ip);
+void parseParams(void);
+void handleRoot(void);
+String sDec(int t); // just 123 to 12.3 string
+String timeFmt(void);
+void handleS(void);
+void handleJson(void);
+void handleEvents(void);
+void handleNotFound(void);
+String dataJson(void);
+
+#endif // WEBHANDLER_H
