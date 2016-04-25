8dada529e48fc4e44cdceb66079746a36c61ad5f
Greg Cunningham <greg@curioustech.net>
2016-04-25 16:44:43 -0400

    Create JsonClient.h

---
+++ Arduino/JsonClient.h
@@ -0,0 +1,54 @@
+/*
+  JsonClient.h - Arduino library for reading JSON data streamed or single request.
+  Copyright 2014 Greg Cunningham, CuriousTech.net
+
+  This library is free software; you can redistribute it and/or modify it under the terms of the GNU GPL 2.1 or later.
+*/
+#ifndef JSONCLIENT_H
+#define JSONCLIENT_H
+
+#include "HVAC.h"
+#include "WiFiClient.h"
+
+enum JC_Status
+{
+    JC_IDLE,
+    JC_CONNECTED,
+    JC_DONE,
+    JC_TIMEOUT,
+    JC_NO_CONNECT,
+};
+
+#define JC_BUF_SIZE 1024
+
+class JsonClient
+{
+public:
+  JsonClient(void (*callback)(uint16_t iEvent, uint16_t iName, uint16_t iValue, char *psValue));
+  bool  addList(const char **pList);
+  bool  begin(const char *pHost, const char *pPath, uint16_t port, bool bKeepAlive);
+  bool  service();
+  void  end(void);
+
+private:
+  bool  connect(void);
+  void  processLine(void);
+  void  sendHeader(const char *pHeaderName, const char *pHeaderValue);
+  void  sendHeader(const char *pHeaderName, int nHeaderValue);
+  void  (*m_callback)(uint16_t iEvent, uint16_t iName, uint16_t iValue, char *psValue);
+
+  WiFiClient m_client;
+  const char *m_pHost;
+  const char *m_pPath;
+  const char **m_jsonList[8];
+  uint16_t m_bufcnt;
+  uint16_t m_event;
+  uint16_t m_Port;
+  char     m_buffer[JC_BUF_SIZE];
+  unsigned long m_timeOut;
+  uint8_t m_jsonCnt;
+  int8_t  m_Status;
+  bool    m_bKeepAlive;
+};
+
+#endif // JSONCLIENT_H
