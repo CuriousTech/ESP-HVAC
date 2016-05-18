5ee8b13cebd204e1d5c23397ad40e93ee06f57bd
Greg Cunningham <greg@curioustech.net>
2016-05-18 01:24:55 -0400

    More remote stuff

--- Arduino/HVAC.h
+++ Arduino/HVAC.h
@@ -1,4 +1,4 @@
-//	HVAC Control
+//  HVAC Control
 //
 #ifndef HVAC_H
 #define HVAC_H
@@ -67,11 +67,11 @@ enum HumidifierMode
 struct EEConfig
 {
   uint16_t size;
-  uint16_t coolTemp[2];	// cool to temp *10 low/high
-  uint16_t heatTemp[2];	// heat to temp *10 low/high
-  int16_t  cycleThresh;	// temp range for cycle *10
+  uint16_t coolTemp[2]; // cool to temp *10 low/high
+  uint16_t heatTemp[2]; // heat to temp *10 low/high
+  int16_t  cycleThresh; // temp range for cycle *10
   uint8_t  Mode;        // Off, Cool, Heat, Auto
-  uint8_t  eHeatThresh;	// degree threshold to switch to gas
+  uint8_t  eHeatThresh; // degree threshold to switch to gas
   uint16_t cycleMin;    // min time to run
   uint16_t cycleMax;    // max time to run
   uint16_t idleMin;     // min time to not run
@@ -93,31 +93,34 @@ public:
   HVAC(void);
   void    init(void);             // after EEPROM read
   void    disable(void);          // Shut it off
-  void	  service(void);          // call once per second
+  void    service(void);          // call once per second
   uint8_t getState(void);         // return current run state simplified (0=off, 1=cool, 2=hp, 4=NG)
   bool    getFanRunning(void);    // return fan running
   uint8_t getMode(void);          // actual mode
   uint8_t getHeatMode(void);      // heat mode
   int8_t  getAutoMode(void);      // get current auto heat/cool mode
   int8_t  getSetMode(void);       // get last requested mode
-  void	  setMode(int8_t mode); 	// request new mode; see enum Mode
+  void    setMode(int8_t mode);   // request new mode; see enum Mode
   void    setHeatMode(uint8_t mode); // heat mode
   bool    getFan(void);           // fan mode
-  void	  setFan(bool on);        // auto/on mode
+  bool    getHumidifierRunning(void);
+  void    setFan(bool on);        // auto/on mode
   void    filterInc(void);
   bool    stateChange(void);      // change since last call = true
   int16_t getSetTemp(int8_t mode, int8_t hl); // get temp set for a mode (cool/heat, hi/lo)
-  void	  setTemp(int8_t mode, int16_t Temp, int8_t hl); // set temp for a mode
-  void    enableRemote(void);
+  void    setTemp(int8_t mode, int16_t Temp, int8_t hl); // set temp for a mode
+  void    enableRemote(uint8_t flags);
   bool    isRemoteTemp(void);
-  void	  updateIndoorTemp(int16_t Temp, int16_t rh);
+  void    updateIndoorTemp(int16_t Temp, int16_t rh);
   void    updateOutdoorTemp(int16_t outTemp);
   void    updatePeaks(int8_t min, int8_t max);
-  void	  resetFilter(void);		// reset the filter hour count
+  void    resetFilter(void);    // reset the filter hour count
   bool    checkFilter(void);
   void    resetTotal(void);
   bool    tempChange(void);
   void    setVar(String sCmd, int val); // remote settings
+  void    updateVar(int iName, int iValue); // host values
+  void    setSettings(int iName, int iValue);// remote settings
   void    enable(void);
   String  settingsJson(void); // get all settings in json format
   String  getPushData(void);  // get states/temps/data in json
@@ -129,24 +132,28 @@ public:
   uint16_t  m_targetTemp;    // end temp for cycle
   uint8_t   m_notif;
   bool      m_bRemoteConnected;
+  bool      m_bRemoteDisconnect;
+  bool      m_bLocalTemp;
   int8_t    m_outMin[2], m_outMax[2];
 
 private:
-  void	fanSwitch(bool bOn);
-  void	tempCheck(void);
+  void  fanSwitch(bool bOn);
+  void  tempCheck(void);
   bool  preCalcCycle(int8_t mode);
   void  calcTargetTemp(int8_t mode);
   int   CmdIdx(String s, const char **pCmds);
-  
-  bool	  m_bFanMode;       // Auto=false, On=true
-  bool	  m_bFanRunning;    // when fan is running
+  void  sendCmd(char *szName, int value);
+
+  bool    m_bFanMode;       // Auto=false, On=true
+  bool    m_bFanRunning;    // when fan is running
+  bool    m_bHumidRunning;
   int8_t  m_AutoMode;       // cool, heat
   int8_t  m_setMode;        // preemted mode request
   int8_t  m_setHeat;        // preemt heat mode request
-  int8_t  m_AutoHeat;	    // auto heat mode choice
-  bool	  m_bRunning;	    // is operating
-  bool	  m_bStart;         // signal to start
-  bool	  m_bStop;          // signal to stop
+  int8_t  m_AutoHeat;     // auto heat mode choice
+  bool    m_bRunning;     // is operating
+  bool    m_bStart;         // signal to start
+  bool    m_bStop;          // signal to stop
   bool    m_bRecheck;       // recalculate target now
   bool    m_bEnabled;       // enables system
   uint16_t m_runTotal;      // time HVAC has been running total since reset
