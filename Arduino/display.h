9841688141345205656b7fd1f7fdbd6df57f4624
Greg Cunningham <greg@curioustech.net>
2016-09-10 12:39:53 -0400

    Changed to points struct

--- Arduino/display.h
+++ Arduino/display.h
@@ -15,6 +15,18 @@ struct Line{
   int16_t y2;
 };
 
+struct gPoint{
+  uint32_t time;
+  uint8_t temp;
+  uint8_t rh;
+  uint8_t l;
+  uint8_t h;
+  uint8_t ltemp;
+  uint8_t state:2;
+  uint8_t fan:1;
+  uint8_t res:5;
+};
+
 class Display
 {
 public:
@@ -33,7 +45,7 @@ public:
   void updateTemps(void);
   void drawForecast(bool bRef);
   void Note(char *cNote);
-  void getGrapthPoints(uint8_t *pts, int n);
+  void getGrapthPoints(gPoint *pt, int n);
 private:
   void refreshAll(void);
   void updateClock(void);
@@ -46,14 +58,15 @@ private:
   void updateRunIndicator(bool bForce); // run and fan running
   void addGraphPoints(void);
   void fillGraph(void);
-  void drawPoints(uint8_t arr, uint16_t color);
+  void drawPoints(uint8_t *arr, uint16_t color);
   void drawPointsTemp(void);
   uint16_t stateColor(uint8_t v);
   void Lines(void);
   int tween(int8_t t1, int8_t t2, int m, int8_t h);
 
   uint16_t m_backlightTimer;
-  uint8_t m_points[300][6]; // Todo: add tamestamp already!
+#define GPTS 300 // 320 px width + 10 padding
+  gPoint m_points[GPTS];
 public:
   uint16_t m_pointsAdded;
   int16_t m_updateFcst = 1;
