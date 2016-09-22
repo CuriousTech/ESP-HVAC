cbfe83d285a182ad59e4785cce2afcc53db8f698
Greg Cunningham <greg@curioustech.net>
2016-09-22 18:44:03 -0400

    Using ESPAsyncWebServer

--- Remote/WebHandler.cpp
+++ Remote/WebHandler.cpp
@@ -4,31 +4,51 @@
 #include <EEPROM.h>
 #include <ESP8266mDNS.h>
 #include "WiFiManager.h"
-#include <ESP8266WebServer.h>
 #include <TimeLib.h> // http://www.pjrc.com/teensy/td_libs_Time.html
 #include "WebHandler.h"
-#include <Event.h>
 #include "HVAC.h"
 #include <JsonClient.h> // https://github.com/CuriousTech/ESP8266-HVAC/tree/master/Libraries/JsonClient
 #include "display.h" // for display.Note()
+#include "pages.h"
 
 //-----------------
 uint8_t serverPort = 86;            // firewalled
 
 //-----------------
-ESP8266WebServer server( serverPort );
+AsyncWebServer server( serverPort );
+AsyncEventSource events("/events"); // event source (Server-Sent events)
 WiFiManager wifi(0);  // AP page:  192.168.4.1
-extern eventHandler event;
 extern HVAC hvac;
 extern Display display;
 
 void startListener(void);
 void getSettings(void);
+void startRemote(void);
 
 void remoteCallback(int16_t iEvent, uint16_t iName, int iValue, char *psValue);
 JsonClient remoteStream(remoteCallback);
 void setCallback(int16_t iEvent, uint16_t iName, int iValue, char *psValue);
 JsonClient remoteSet(setCallback);
+void handleChart(AsyncWebServerRequest *request);
+
+void onBody(AsyncWebServerRequest *request, uint8_t *data, size_t len, size_t index, size_t total){
+  //Handle body
+}
+
+void onUpload(AsyncWebServerRequest *request, String filename, size_t index, uint8_t *data, size_t len, bool final){
+  //Handle upload
+}
+
+void onRequest(AsyncWebServerRequest *request){
+  //Handle Unknown Request
+  request->send(404);
+}
+
+void onEvents(AsyncEventSourceClient *client)
+{
+//  client->send(":ok", NULL, millis(), 1000);
+  events.send(dataJson().c_str(), "state");
+}
 
 void startServer()
 {
@@ -44,27 +64,76 @@ void startServer()
     Serial.println ( "MDNS responder failed" );
   }
 
-  server.on ( "/", handleRoot );
-  server.on ( "/events", handleEvents );
-  server.onNotFound ( handleNotFound );
+  // attach AsyncEventSource
+  events.onConnect(onEvents);
+  server.addHandler(&events);
+
+  server.on ( "/", HTTP_GET | HTTP_POST, handleRoot );
+  server.on ( "/json", HTTP_GET | HTTP_POST, handleJson );
+  server.on ( "/chart.html", HTTP_GET, handleChart );
+
+  server.onNotFound(onRequest);
+  server.onFileUpload(onUpload);
+  server.onRequestBody(onBody);
+
   server.begin();
   // Add service to MDNS-SD
   MDNS.addService("http", "tcp", serverPort);
 }
 
+void handleChart(AsyncWebServerRequest *request)
+{
+  AsyncResponseStream *response = request->beginResponseStream("text/html");
+
+  String out = String(chart1); // Todo: Get from PROGMEM correctly
+
+  gPoint gpt;
+
+  int32_t tb = 0;
+
+  for(int x = 0; x < GPTS; x++)
+  {
+    if( display.getGrapthPoints(&gpt, x) == false)
+      break;
+    if(gpt.time == 0)
+      continue; // some glitch?
+    if(tb == 0) // first entry found
+    {
+      tb = gpt.time - (60*60*26);
+      out += tb;
+      out += "\ndata = { values:[\n";
+    }
+    out += "{t:";
+    out += gpt.time - tb;
+    out += ",temp:\"";
+    out += String((float)(gpt.temp * 110 / 101 + 660)/10, 1);
+    out += "\",rh:\"";
+    out += String((float)(gpt.rh * 250 / 55) / 10, 1);
+    out += "\",h:\"";
+    out += String((float)(gpt.h * 110 / 101 + 660)/10, 1);
+    out += "\",l:\"";
+    out += String((float)(gpt.l * 110 / 101 + 660)/10, 1);
+    out += "\",s:";
+    out += gpt.state;
+    out += ",f:";
+    out += gpt.fan;
+    out += "},\n";
+  }
+  response->print(out);
+  response->print(String(chart2));
+
+  request->send ( response );
+}
+
 void handleServer()
 {
   MDNS.update();
-  server.handleClient();
-  remoteStream.service();
-  remoteSet.service();
 }
 
 void secondsServer() // called once per second
 {
   if(hvac.tempChange())
-    event.pushInstant();
-  else event.heartbeat();
+    events.send(dataJson().c_str(), "state");
 
   static uint8_t cnt = 5;
   if(--cnt == 0)
@@ -73,7 +142,7 @@ void secondsServer() // called once per second
     getSettings();
   }
 
-  static uint8_t start = 10; // give it time to settle before initial connect
+  static uint8_t start = 15; // give it time to settle before initial connect
   if(start)
   {
     if(--start == 0)
@@ -86,20 +155,21 @@ void secondsServer() // called once per second
   }
 }
 
-void parseParams()
+void parseParams(AsyncWebServerRequest *request)
 {
   char temp[100];
   int val;
 
 //  Serial.println("parseArgs");
 
-  for ( uint8_t i = 0; i < server.args(); i++ ) {
-    server.arg(i).toCharArray(temp, 100);
+  for ( uint8_t i = 0; i < request->params(); i++ ) {
+    AsyncWebParameter* p = request->getParam(i);
+    p->value().toCharArray(temp, 100);
     String s = wifi.urldecode(temp);
 //    Serial.println( i + " " + server.argName ( i ) + ": " + s);
     int val = s.toInt();
  
-    switch( server.argName(i).charAt(0)  )
+    switch( p->name().charAt(0)  )
     {
       case 'F': // temp offset
           hvac.m_EE.adj = val;
@@ -112,6 +182,19 @@ void parseParams()
             startListener(); // reset the URI
           }
           break;
+      case 'R': // remote
+          if(hvac.m_EE.bLock) break;
+          if(val)
+          {
+            if(hvac.m_bRemoteStream == false)
+              hvac.enableRemote(); // enable
+          }
+          else
+          {
+            if(hvac.m_bRemoteStream == true)
+              hvac.enableRemote(); // disable
+          }
+          break;
       case 'P': // host port
           hvac.m_EE.hostPort = s.toInt();
           startListener();
@@ -120,10 +203,10 @@ void parseParams()
   }
 }
 
-void handleRoot() // Main webpage interface
+void handleRoot(AsyncWebServerRequest *request) // Main webpage interface
 {
 //  Serial.println("handleRoot");
-  parseParams();
+  parseParams(request);
 
   String page = 
    "<!DOCTYPE html>\n"
@@ -149,50 +232,15 @@ void handleRoot() // Main webpage interface
    "</body>\n"
    "</html>\n";
 
-  server.send ( 200, "text/html", page );
+  request->send( 200, "text/html", page );
 }
 
-// event streamer (assume keep-alive)
-void handleEvents()
+// Return lots of vars as JSON
+void handleJson(AsyncWebServerRequest *request)
 {
-  char temp[100];
-//  Serial.println("handleEvents");
-  uint16_t interval = 60; // default interval
-  uint8_t nType = 0;
-  String key;
-
-  for ( uint8_t i = 0; i < server.args(); i++ ) {
-    server.arg(i).toCharArray(temp, 100);
-    String s = wifi.urldecode(temp);
-//    Serial.println( i + " " + server.argName ( i ) + ": " + s);
-    int val = s.toInt();
- 
-    switch( server.argName(i).charAt(0)  )
-    {
-      case 'i': // interval
-        interval = val;
-        break;
-      case 'p': // push
-        nType = 1;
-        break;
-      case 'c': // critical
-        nType = 2;
-        break;
-      case 'k': // key
-        key = s;
-        break;
-    }
-  }
-
-  if(nType == 2 && key != hvac.m_EE.password) // demote to plain if no/incorrect password
-    nType = 0;
-
-  String content = "HTTP/1.1 200 OK\n"
-      "Connection: keep-alive\n"
-      "Access-Control-Allow-Origin: *\n"
-      "Content-Type: text/event-stream\n\n";
-  server.sendContent(content);
-  event.set(server.client(), interval, nType);
+//  Serial.println("handleJson\n");
+  String s = hvac.settingsJson();
+  request->send( 200, "text/json", s + "\n");
 }
 
 // Pushed data
@@ -216,8 +264,14 @@ void remoteCallback(int16_t iEvent, uint16_t iName, int iValue, char *psValue)
       {
         if(bConn)
         {
-          event.print(String("remote error ") + iName + " " + psValue + ":" + iValue);
-          event.alert("Remote link disconnected");
+          String s = String("remote error ");
+          s += iName;
+          s += " ";
+          s += psValue;
+          s += ":";
+          s += iValue;
+          events.send(s.c_str(), "print");
+          events.send("Remote link disconnected", "alert");
           hvac.m_notif = Note_Network;
           bConn = false;
           hvac.m_bLocalTempDisplay = true;
@@ -225,7 +279,7 @@ void remoteCallback(int16_t iEvent, uint16_t iName, int iValue, char *psValue)
       }
       else if(!bConn)
       {
-        event.alert("Remote link connected");
+//        event.alert("Remote link connected");
         hvac.m_bLocalTempDisplay = false;
         bConn = true;
         if(hvac.m_notif == Note_Network)
@@ -243,15 +297,8 @@ void remoteCallback(int16_t iEvent, uint16_t iName, int iValue, char *psValue)
 
 void startListener()
 {
-//  static char path[] = "/events?i=30&p=1";
-
-  String path = "/events?key=";
-  path += hvac.m_EE.password;
-  path += "&i=30&p=1&s=%2Fevents%3Fi=30%26p=1&r=";  // the path needs to be URL encoded
-  path += serverPort;
-
   IPAddress ip(hvac.m_EE.hostIp);
-  remoteStream.begin(ip.toString().c_str(), path.c_str(), hvac.m_EE.hostPort, true);
+  remoteStream.begin(ip.toString().c_str(), "/events", hvac.m_EE.hostPort, true);
   remoteStream.addList(jsonList1);
   remoteStream.addList(jsonList2);
 }
@@ -265,7 +312,15 @@ void setCallback(int16_t iEvent, uint16_t iName, int iValue, char *psValue)
   {
     case -1:
       if(iName != JC_CONNECTED)
-        event.print(String("getSettings error ") + iName + " " + psValue + ":" + iValue);
+      {
+        String s = String("getSettings error ");
+        s += iName;
+        s += " ";
+        s += psValue;
+        s += ":";
+        s += iValue;
+        events.send(s.c_str(), "print");
+      }
       break;
     case 0: // settings
       hvac.setSettings(iName, iValue);
@@ -275,27 +330,18 @@ void setCallback(int16_t iEvent, uint16_t iName, int iValue, char *psValue)
 
 void getSettings()
 {
-  static char path[] = "/json";
-  IPAddress ip(hvac.m_EE.hostIp);
-  remoteSet.begin(ip.toString().c_str(), path, hvac.m_EE.hostPort, false);
-  remoteSet.addList(jsonList3);
-}
+  String path = "/json";
 
-void handleNotFound() {
-//  Serial.println("handleNotFound");
-
-  String message = "File Not Found\n\n";
-  message += "URI: ";
-  message += server.uri();
-  message += "\nMethod: ";
-  message += ( server.method() == HTTP_GET ) ? "GET" : "POST";
-  message += "\nArguments: ";
-  message += server.args();
-  message += "\n";
-
-  for ( uint8_t i = 0; i < server.args(); i++ ) {
-    message += " " + server.argName ( i ) + ": " + server.arg ( i ) + "\n";
+  static bool bInit = false;
+  if(bInit == false) // send eventlistener data only once
+  {
+    bInit = true;
+    path += "?key=";
+    path += hvac.m_EE.password;
+    path += "&port=";
+    path += serverPort;
   }
-
-  server.send ( 404, "text/plain", message );
+  IPAddress ip(hvac.m_EE.hostIp);
+  remoteSet.begin(ip.toString().c_str(), path.c_str(), hvac.m_EE.hostPort, false);
+  remoteSet.addList(jsonList3);
 }
