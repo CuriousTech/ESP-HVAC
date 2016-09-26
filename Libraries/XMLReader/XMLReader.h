238d16f27cfdd83fbabd985e2bd68c19d6031a63
Greg Cunningham <greg@curioustech.net>
2016-09-26 00:46:50 -0400

    Update XMLReader.h

--- Libraries/XMLReader/XMLReader.h
+++ Libraries/XMLReader/XMLReader.h
@@ -2,16 +2,13 @@
 #define XMLREADER_H
 
 #include <Arduino.h>
-#include "WiFiClient.h"
+#include <ESPAsyncTCP.h>
 
 enum XML_Status
 {
-  XML_IDLE,
-  XML_CONNECTED,
   XML_DONE,
-  XML_TIMEOUT,
-  XML_NO_CONNECT,
-  XML_TRUNCATE
+  XML_COMPLETED,
+  XML_TIMEOUT
 };
 
 struct XML_tag_t
@@ -25,39 +22,44 @@ struct XML_tag_t
 class XMLReader
 {
 public:
-  XMLReader(char *pBuffer, int16_t bufSize, void (*xml_callback)(int8_t item, int8_t idx, char *p));
-  bool  begin(const char *pHost, char *pPath[]);
-  bool  service(XML_tag_t *tags);
-  void  end(void);
-  int8_t getStatus(void);
-    
+  XMLReader(void (*xml_callback)(int8_t item, int8_t idx, char *p), XML_tag_t *pTags);
+  bool  begin(const char *pHost, String path);
+
 private:
   bool  combTag(const char *pTagName, const char *pAttr, const char *pValue);
   bool  nextValue(XML_tag_t *tags);
-  bool  fillBuffer(void);
+  bool  fillBuffer(char* data, size_t len);
   void  emptyBuffer(void);
   void  sendHeader(const char *pHeaderName, const char *pHeaderValue);
   void  sendHeader(const char *pHeaderName, int nHeaderValue);
   bool  tagCompare(char *p1, const char *p2);
   void  IncPtr(void);
   bool  tagStart(void);
+  int   tagCnt(void);
   bool  tagEnd(void);
 
   void  (*m_xml_callback)(int8_t item, int8_t idx, char *p);
 
-  WiFiClient m_client;
+  AsyncClient m_client;
 
-  char   *m_pBuffer;
+  void _onConnect(AsyncClient* client);
+  void _onDisconnect(AsyncClient* client);
+  static void _onError(AsyncClient* client, int8_t error);
+  void _onTimeout(AsyncClient* client, uint32_t time);
+  void _onData(AsyncClient* client, char* data, size_t len);
+
+  const char  *m_pHost;
+  char m_buffer[257];
+  String m_path;
+  XML_tag_t *m_pTags;
   char   *m_pPtr;
   char   *m_pEnd;
   char   *m_pIn;
   const char *m_pTagName;
-  bool    m_binValues;
-  int8_t  m_tagIdx;
-  int8_t  m_tagState;
-  int8_t  m_valIdx;
-  int8_t  m_Status;
-  unsigned long m_timeOut;
+  bool   m_binValues;
+  int8_t m_tagIdx;
+  int8_t m_tagState;
+  int8_t m_valIdx;
 };
 
 #endif // XMLREADER_H
