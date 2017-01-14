6a9d03c38cf5d2e6821dc83ff9f472f60421690b
Greg Cunningham <greg@curioustech.net>
2017-01-14 13:10:23 -0500

    Added required key for parseParams

--- Arduino/WiFiManager.h
+++ Arduino/WiFiManager.h
@@ -28,7 +28,7 @@ class WiFiManager
 public:
     WiFiManager();
     void autoConnect(void);
-    void autoConnect(char const *apName);
+    void autoConnect(char const *apName, const char *pPass);
     String page(void);
     void seconds(void);
     void setSSID(int idx);
@@ -39,6 +39,7 @@ private:
     boolean hasConnected();
 
     const char* _apName = "no-net";
+    const char *_pPass = "";
     bool _timeout;
     bool _bCfg;
 
@@ -47,7 +48,7 @@ private:
     const String HTTP_SCRIPT = "<script>function c(l){document.getElementById('s').value=l.innerText||l.textContent;document.getElementById('p').focus();}</script>";
     const String HTTP_HEAD_END = "</head><body>";
     const String HTTP_ITEM = "<div><a href='#' onclick='c(this)'>{v}</a></div>";
-    const String HTTP_FORM = "<form method='get' action='/'><input id='s' name='ssid' length=32 placeholder='SSID'><input id='p' name='pass' length=64 placeholder='password'><br/><input type='submit'></form>";
+    const String HTTP_FORM = "<form method='get' action='s'><input id='s' name='ssid' length=32 placeholder='SSID'><input id='p' name='pass' length=64 placeholder='password'><input type='hidden' name='key' value='$key' ><br/><input type='submit'></form>";
     const String HTTP_END = "</body></html>";
 };
 
