f29a3f54ed9833916edef392ae3c6ece78009268
Greg Cunningham <greg@curioustech.net>
2020-08-08 01:09:53 -0400

    Update WebHandler.h

--- Arduino/WebHandler.h
+++ Arduino/WebHandler.h
@@ -12,12 +12,12 @@ void parseParams(AsyncWebServerRequest *request);
 String sDec(int t); // just 123 to 12.3 string
 String timeFmt(void);
 String dataJson(void);
-void WsSend(char *txt, const char *type);
+void WsSend(String s);
 void fc_onConnect(AsyncClient* client);
 void fc_onData(AsyncClient* client, char* data, size_t len);
 void fc_onDisconnect(AsyncClient* client);
 void fc_onTimeout(AsyncClient* client, uint32_t time);
 void historyDump(bool bStart);
 void appendDump(int startTime);
-
+String forecastJson(void);
 #endif // WEBHANDLER_H
