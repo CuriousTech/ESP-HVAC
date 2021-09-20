3ba3da4399e5f4eff0b37e440f17c1753bfb5f52
Greg Cunningham <greg@curioustech.net>
2021-09-20 04:41:05 -0400

    Update JsonParse.h

--- Libraries/JsonParse/JsonParse.h
+++ Libraries/JsonParse/JsonParse.h
@@ -1,7 +1,6 @@
 /*
   JsonParse.h - Arduino library for reading JSON data.
   Copyright 2016 Greg Cunningham, CuriousTech.net
-
   This library is free software; you can redistribute it and/or modify it under the terms of the GNU GPL 2.1 or later.
 */
 #ifndef JSONPARSE_H
@@ -20,8 +19,8 @@ private:
   char *skipwhite(char *p);
   void  (*m_callback)(int16_t iEvent, uint16_t iName, int iValue, char *psValue);
 
-#define LIST_CNT
-  const char **m_jsonList[8];
+#define LIST_CNT 8
+  const char **m_jsonList[LIST_CNT];
   uint8_t m_jsonCnt;
 };
 
