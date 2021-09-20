7fb3b8f48fa5700261b3a1301631ca1c9a9f835c
Greg Cunningham <greg@curioustech.net>
2021-09-20 04:41:37 -0400

    Update JsonParse.cpp

--- Libraries/JsonParse/JsonParse.cpp
+++ Libraries/JsonParse/JsonParse.cpp
@@ -1,9 +1,7 @@
 /*
   JsonParse.h - Arduino library for parsing JSON data.  Note: Data string is written to for NULL termination.
   Copyright 2016 Greg Cunningham, CuriousTech.net
-
   This library is free software; you can redistribute it and/or modify it under the terms of the GNU GPL 2.1 or later.
-
   8 lists max per instance
 */
 #include "JsonParse.h"
@@ -18,7 +16,7 @@ JsonParse::JsonParse(void (*callback)(int16_t iEvent, uint16_t iName, int iValue
 // add a json list {"event name", "valname1", "valname2", "valname3", NULL}
 bool JsonParse::addList(const char **pList)
 {
-  if(m_jsonCnt >= 8)
+  if(m_jsonCnt >= LIST_CNT)
     return false;
   m_jsonList[m_jsonCnt++] = pList;
   return true;
@@ -53,10 +51,8 @@ void JsonParse::process(char *event, char *data)
     {
        while(*p && *p!= '"') p++;
        if(*p == '"') *p++ = 0;
-    }else
-    {
-      while(*p && *p != ':') p++;
     }
+    while(*p && *p != ':') p++;
     if(*p != ':') return;
     *p++ = 0;
     p = skipwhite(p);
