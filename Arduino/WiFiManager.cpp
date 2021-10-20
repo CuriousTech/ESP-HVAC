1b5abd91d3efab8b47e364a38828a980057b81c6
Greg Cunningham <greg@curioustech.net>
2021-10-20 12:13:18 -0400

    Update WiFiManager.cpp

--- Arduino/WiFiManager.cpp
+++ Arduino/WiFiManager.cpp
@@ -179,39 +179,3 @@ String WiFiManager::page()
   s += HTTP_END;
   return s;
 }
-
-String WiFiManager::urldecode(const char *src)
-{
-    String decoded = "";
-    char a, b;
-    while (*src) {
-        if ((*src == '%') &&
-            ((a = src[1]) && (b = src[2])) &&
-            (isxdigit(a) && isxdigit(b))) {
-            if (a >= 'a')
-                a -= 'a'-'A';
-            if (a >= 'A')
-                a -= ('A' - 10);
-            else
-                a -= '0';
-            if (b >= 'a')
-                b -= 'a'-'A';
-            if (b >= 'A')
-                b -= ('A' - 10);
-            else
-                b -= '0';
-            
-            decoded += char(16*a+b);
-            src+=3;
-        } else if (*src == '+') {
-            decoded += ' ';
-            *src++;
-        } else {
-            decoded += *src;
-            *src++;
-        }
-    }
-    decoded += '\0';
-    
-    return decoded;
-}
