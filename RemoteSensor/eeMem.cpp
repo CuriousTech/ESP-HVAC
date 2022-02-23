a332e5d755f542a090236cc30cfacf9582dcb916
Greg Cunningham <greg@curioustech.net>
2022-02-23 09:20:01 -0500

    Simplified a bit

--- RemoteSensor/eeMem.cpp
+++ RemoteSensor/eeMem.cpp
@@ -1,60 +1,39 @@
 #include "eeMem.h"
 #include <EEPROM.h>
 
-eeSet ee = { sizeof(eeSet), 0xAAAA,
-  "",  // saved SSID
-  "", // router password
-  -5,     // TZ
-  {0,0,1,1,0,0,0}, // PirEn, bPIR, bCall, bCF, bUseTime, bEnableOLED, res
-  "Sensor1",
-  '1SNS',
-  0,  // adjust for error
-  15, // sendRate
-  60, // logRate
-  "password", // device password for control
-  {192,168,31,100}, 80, // host IP and port
-  {192,168,31,125}, // HVAC IP
-  0, // minutes off
-  30, // sleep
-  60*5, // en/priority seconds
-  12, // pirPin
-  {320, 1000, 0, 900, 0, 1000, 0, 10, 0, 20, 0, 1000, 0, 1000, 0, 1000}, // alert levels
-  {0},
-};
-
 void eeMem::init()
 {
-  EEPROM.begin(512);
+  EEPROM.begin(EESIZE);
 
-  uint8_t data[sizeof(eeSet)];
+  uint8_t data[EESIZE];
   uint16_t *pwTemp = (uint16_t *)data;
 
   int addr = 0;
-  for(int i = 0; i < sizeof(eeSet); i++, addr++)
+  for(int i = 0; i < EESIZE; i++, addr++)
   {
     data[i] = EEPROM.read( addr );
   }
 
-  if(pwTemp[0] != sizeof(eeSet)) return; // revert to defaults if struct size changes
+  if(pwTemp[0] != EESIZE) return; // revert to defaults if struct size changes
   uint16_t sum = pwTemp[1];
   pwTemp[1] = 0;
-  pwTemp[1] = Fletcher16(data, sizeof(eeSet) );
+  pwTemp[1] = Fletcher16(data, EESIZE );
   if(pwTemp[1] != sum) return; // revert to defaults if sum fails
-  memcpy(&ee, data, sizeof(eeSet) );
+  memcpy(this + offsetof(eeMem, size), data, EESIZE );
 }
 
 void eeMem::update() // write the settings if changed
 {
   uint16_t old_sum = ee.sum;
   ee.sum = 0;
-  ee.sum = Fletcher16((uint8_t*)&ee, sizeof(eeSet));
+  ee.sum = Fletcher16((uint8_t*)this + offsetof(eeMem, size), EESIZE);
 
   if(old_sum == ee.sum)
     return; // Nothing has changed?
 
   uint16_t addr = 0;
-  uint8_t *pData = (uint8_t *)&ee;
-  for(int i = 0; i < sizeof(eeSet); i++, addr++)
+  uint8_t *pData = (uint8_t *)this + offsetof(eeMem, size);
+  for(int i = 0; i < EESIZE; i++, addr++)
   {
     EEPROM.write(addr, pData[i] );
   }
