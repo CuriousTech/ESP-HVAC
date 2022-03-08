143d464204d96026bbbe8cea767d5c4fc2c53d5e
Greg Cunningham <greg@curioustech.net>
2022-03-08 18:40:37 -0500

    Removed old decls

--- Arduino/WebHandler.h
+++ Arduino/WebHandler.h
@@ -7,16 +7,9 @@
 void startServer(void);
 bool handleServer(void);
 void secondsServer(void);
-String ipString(IPAddress ip);
 void parseParams(AsyncWebServerRequest *request);
-String sDec(int t); // just 123 to 12.3 string
-String timeFmt(void);
 String dataJson(void);
 void WsSend(String s);
-void fc_onConnect(AsyncClient* client);
-void fc_onData(AsyncClient* client, char* data, size_t len);
-void fc_onDisconnect(AsyncClient* client);
-void fc_onTimeout(AsyncClient* client, uint32_t time);
 void historyDump(bool bStart);
 void appendDump(uint32_t startTime);
 
